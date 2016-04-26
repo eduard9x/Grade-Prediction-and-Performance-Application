82dd5827f8327413fab64d3bdc271ab18baf1c18
Eduard <ghinea.eduard@yahoo.com>
2016-04-26 13:57:46 +0300

    added project on Ion's PC. Created first screen properly. added database. Need to start adding to DB on start button.

--- WindowsFormsApplication2/Views/StartPage.Designer.cs
+++ WindowsFormsApplication2/Views/StartPage.Designer.cs
@@ -31,24 +31,27 @@
             this.nameBox = new System.Windows.Forms.TextBox();
             this.startButton = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.courseLabel = new System.Windows.Forms.Label();
+            this.courseBox = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // nameBox
             // 
-            this.nameBox.Location = new System.Drawing.Point(177, 239);
+            this.nameBox.Location = new System.Drawing.Point(177, 191);
             this.nameBox.Name = "nameBox";
-            this.nameBox.Size = new System.Drawing.Size(175, 20);
+            this.nameBox.Size = new System.Drawing.Size(180, 20);
             this.nameBox.TabIndex = 0;
             this.nameBox.TextChanged += new System.EventHandler(this.nameBox_TextChanged);
             // 
             // startButton
             // 
-            this.startButton.ForeColor = System.Drawing.Color.Fuchsia;
+            this.startButton.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.ForeColor = System.Drawing.Color.Crimson;
             this.startButton.Location = new System.Drawing.Point(177, 282);
             this.startButton.Name = "startButton";
-            this.startButton.Size = new System.Drawing.Size(175, 23);
-            this.startButton.TabIndex = 1;
+            this.startButton.Size = new System.Drawing.Size(180, 33);
+            this.startButton.TabIndex = 2;
             this.startButton.Text = "Start";
             this.startButton.UseVisualStyleBackColor = true;
             this.startButton.Click += new System.EventHandler(this.startButton_Click);
@@ -64,16 +67,36 @@
             this.label1.Text = "Grade Prediction Application";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
-            // label2
+            // nameLabel
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.label2.Location = new System.Drawing.Point(173, 216);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(176, 20);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Please enter your name";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.nameLabel.Location = new System.Drawing.Point(173, 168);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(182, 20);
+            this.nameLabel.TabIndex = 3;
+            this.nameLabel.Text = "Please enter your name*";
+            this.nameLabel.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // courseLabel
+            // 
+            this.courseLabel.AutoSize = true;
+            this.courseLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.courseLabel.Location = new System.Drawing.Point(173, 225);
+            this.courseLabel.Name = "courseLabel";
+            this.courseLabel.Size = new System.Drawing.Size(190, 20);
+            this.courseLabel.TabIndex = 4;
+            this.courseLabel.Text = "Please enter your course*";
+            this.courseLabel.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // courseBox
+            // 
+            this.courseBox.Location = new System.Drawing.Point(177, 248);
+            this.courseBox.Name = "courseBox";
+            this.courseBox.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.courseBox.Size = new System.Drawing.Size(180, 20);
+            this.courseBox.TabIndex = 1;
+            this.courseBox.TextChanged += new System.EventHandler(this.courseBoxChanged);
             // 
             // StartPage
             // 
@@ -81,7 +104,9 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.SeaGreen;
             this.ClientSize = new System.Drawing.Size(539, 517);
-            this.Controls.Add(this.label2);
+            this.Controls.Add(this.courseBox);
+            this.Controls.Add(this.courseLabel);
+            this.Controls.Add(this.nameLabel);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.startButton);
             this.Controls.Add(this.nameBox);
@@ -98,6 +123,8 @@
         private System.Windows.Forms.TextBox nameBox;
         private System.Windows.Forms.Button startButton;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.Label courseLabel;
+        private System.Windows.Forms.TextBox courseBox;
     }
 }
--- WindowsFormsApplication2/Views/StartPage.cs
+++ WindowsFormsApplication2/Views/StartPage.cs
@@ -13,6 +13,7 @@ namespace GradePredictionApplication.Views
     public partial class StartPage : Form
     {
         String name = "Visitor";
+        String course = "default";
         EntryPointController bla;
 
         public StartPage()
@@ -43,16 +44,39 @@ namespace GradePredictionApplication.Views
 
         private void startButton_Click(object sender, EventArgs e)
         {
-            Console.WriteLine(" <<<< START with name: " + name);
-            //this.Hide();
-            //todo NEED to close application on exit of other forms
-            bla.EntryPointView();
+            if (name == "Visitor" || name.Length==0) nameLabel.ForeColor = System.Drawing.Color.Red;
+            else
+            {
+                nameLabel.ForeColor = System.Drawing.Color.Blue;
+
+                if (course == "default" || course.Length == 0) courseLabel.ForeColor = System.Drawing.Color.Red;
+                else
+                {
+                    courseLabel.ForeColor = System.Drawing.Color.Blue;
+                    Console.WriteLine(" <<<< START with name: " + name);
+                    //this.Hide();
+                    //todo NEED to close application on exit of other forms
+                    bla.EntryPointView();
+                }
+            }
+            
         }
 
         private void nameBox_TextChanged(object sender, EventArgs e)
         {
             TextBox nameHolder = (TextBox) sender;
             name = nameHolder.Text;
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void courseBoxChanged(object sender, EventArgs e)
+        {
+            TextBox courseHolder = (TextBox)sender;
+            course = courseHolder.Text;
         }
     }
 }
